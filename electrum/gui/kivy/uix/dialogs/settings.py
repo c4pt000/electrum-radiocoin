9f45400751c469a3ad6a87cb03143bd2b98aa088
Your Name <you@example.com>
2021-10-25 06:59:09 -0400

    changes to build-for-android script, changes to "about" screen,

--- electrum/gui/kivy/uix/dialogs/settings.py
+++ electrum/gui/kivy/uix/dialogs/settings.py
@@ -55,7 +55,7 @@ Builder.load_string('''
                 SettingsItem:
                     bu: app.base_unit
                     title: _('Denomination') + ': ' + self.bu
-                    description: _("Base unit for UraniumX amounts.")
+                    description: _("Base unit for Radiocoin amounts.")
                     action: partial(root.unit_dialog, self)
                 CardSeparator
                 SettingsItem:
