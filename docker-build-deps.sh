191cdc800539305d610ccece5dff8cd42d3f7cb6
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2021-12-12 13:33:12 -0500

    Update docker-build-deps.sh

--- docker-build-deps.sh
+++ docker-build-deps.sh
@@ -9,8 +9,16 @@ docker run -it --net host -d --rm -e "DISPLAY=${DISPLAY:-:0.0}" -v /tmp/.X11-uni
 
 echo '
 apt install net-tools -y
+cd /home/user/wspace/electrum
+
      apt-get update
      pip install -r contrib/deterministic-build/requirements-hw.txt 
-     apt install libsecp256k1-dev mesa-* qt5-default x11-* libzbar-dev -y
-     pip install PyQt5 opencv-python
+     apt install libsecp256k1-dev mesa-* qt5-default x11-* libzbar-dev xclip nano xserver-video-intel -y
+     pip install PyQt5 opencv-python cython kivy
 '
+
+
+
+
+
+   
